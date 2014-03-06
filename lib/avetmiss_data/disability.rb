db2606aeac6c9890bad44f55ca72c11386f5d61a
Jehanzeb Khan <jehanzebk@jobready.com.au>
2014-03-06 15:15:15 +1100

    Added NAT00090 + specs.

---
+++ lib/avetmiss_data/disability.rb
@@ -0,0 +1,9 @@
+class AvetmissData::Disability < AvetmissData::Base
+
+  self.file = 'NAT00090'
+  self.file_format = {
+    client_identifier: 0...10,
+    disability_type_identifier: 10...12,
+    extras: 12..-1
+  }
+end
