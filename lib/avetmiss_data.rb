8edcd4112f77e0be8108f4a3e227592a19512460
Brenton "B-Train" Fletcher <i@bloople.net>
2014-03-11 19:45:01 +1100

    Basic zip file support

--- lib/avetmiss_data.rb
+++ lib/avetmiss_data.rb
@@ -1,4 +1,5 @@
 require 'active_support/all'
+require 'zipruby'
 
 module AvetmissData
   module AvetmissData::Stores
--- spec/zip_file_spec.rb
+++ spec/zip_file_spec.rb
@@ -2,7 +2,16 @@ require 'spec_helper'
 
 describe AvetmissData::ZipFile do
   context 'read a zip file' do
-    let!(:zip_file) { build :zip_file, io: File.open('spec/fixtures/zip_files/valid.zip') }
+    let!(:zip_file) { AvetmissData::ZipFile.new('spec/fixtures/zip_files/valid.zip') }
 
+    context do
+      specify { expect(zip_file.stores).not_to be_empty }
+      let(:nat_10s) { zip_file.stores["NAT00010"] }
+      specify { expect(nat_10s).not_to be_empty }
+      specify { expect(nat_10s.length).to eq(1) }
+      let(:nat_10) { nat_10s.first }
+      specify { expect(nat_10.training_organisation_identifier).to eq('0101') }
+
+    end
   end
 end
