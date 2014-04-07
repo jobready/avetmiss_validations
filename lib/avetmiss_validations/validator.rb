cef0d49ecfbbb37805064f4ef3ab45628b70b90d
Brenton "B-Train" Fletcher <i@bloople.net>
2014-04-07 16:48:16 +1000

    Add #results

--- lib/avetmiss_validations/validator.rb
+++ lib/avetmiss_validations/validator.rb
@@ -8,7 +8,6 @@
 # @attr [AvetmissData::Stores::Base] store The line in a NAT file whose attributes we are validating.
 # @attr [Array] fatals The list of fatal errors that occured.
 # @attr [Array] warnings The list of warnings that occured
-
 class AvetmissValidations::Validator
   attr_accessor :store, :fatals, :warnings
 
@@ -21,6 +20,10 @@ class AvetmissValidations::Validator
 
   def validate
     raise "Subclass must implement #validate"
+  end
+
+  def results
+    fatals + warnings
   end
 
   def fatals?
--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe AvetmissValidations::Validator do
-  let(:base_store) { AvetmissData::Stores::Base.new()}
+  before { allow_any_instance_of(described_class).to receive(:validate).and_return(nil) }
+  let(:validator) { described_class.new(nil) }
+
+  context '#results' do
+    before do
+      validator.fatals << :a
+      validator.warnings << :b
+    end
+
+    specify { expect(validator.results).to eq([:a, :b]) }
+  end
 end
